f227f69d06a6fcba472d0f569d0d00a7bef71d75
Yaroslavik <yaroslav4167@mail.ru>
2021-02-08 17:40:49 +0200

    Remove unnecessary code

--- StartBot.py
+++ StartBot.py
@@ -10,7 +10,6 @@ class MyClient(discord.Client):
 		
 	def get_from(self, url):
 		headers = {'X-Requested-With': 'XMLHttpRequest', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'}
-		req = requests.get(url, headers=headers, timeout=3)
 		try:
 			req = requests.get(url, headers=headers, timeout=15)
 		except Timeout:
