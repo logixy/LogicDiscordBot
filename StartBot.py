29343f3b67025d1fff7a03b76809230b222720fb
Yaroslavik <yaroslav4167@mail.ru>
2021-04-28 23:04:35 +0200

    Fix special chars on command (comma, slash etc.)

--- StartBot.py
+++ StartBot.py
@@ -61,7 +61,8 @@ class MyClient(discord.Client):
 
         message.content = message.content.lower()
         # Перевод сообщения написанного в английской раскладке
-        message.content = self.convert_qwe_message(message.content)
+        if not (message.content[0] in "йцукенгшщзхъфывапролджэячсмитьбю"):
+            message.content = self.convert_qwe_message(message.content)
         # print(message.content)
         if(message.content == '<@!' + str(self.user.id) + '>'):
             text = 'Что Вам необходимо?\n ' + \
